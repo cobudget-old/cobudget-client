f59a723e3752316ac0aefa4f87a7cc160fdc6205
euglazer <eugenestapler@gmail.com>
2016-03-25 04:23:36 -0700

    getting everything but users

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var request = require('request')
 var prompt = require('prompt')
 var pick = require('lodash.pick')
+var async = require('async')
 
 var params;
 
@@ -27,10 +28,28 @@ function CobudgetClient (params) {
   }, function (err, response, body) {
     var headers = response.toJSON().headers
     self.authHeaders = pick(headers, ['access-token', 'uid', 'client', 'token-type'])
-    self.getMe(function (err, me) {
-      console.log('me: ', me)
-    })
-    // test get
+    async.parallel({
+      me: self.getMe.bind(self),
+      group: async.apply(self.getGroup.bind(self), 41),
+      buckets: function (callback) {
+        self.getBucketsForGroup({ id: 41 }, function (err, buckets) {
+          if (err) return callback(err);
+          async.map(buckets, function (bucket, cb) {
+            async.parallel({
+              comments: async.apply(self.getCommentsForBucket.bind(self), bucket),
+              contributions: async.apply(self.getContributionsForBucket.bind(self), bucket)
+            }, function (err, results) {
+              bucket.contributions = results.contributions
+              bucket.comments = results.comments
+              cb(null, bucket)
+            })
+          }, callback)
+        });
+      },
+      allocations: async.apply(self.getAllocationsForGroup.bind(self), { id: 41 })
+    }, function (err, results) {
+      console.log('results: ', JSON.stringify(results))
+    });
   })
 }
 
@@ -41,6 +60,61 @@ CobudgetClient.prototype.getMe = function (callback) {
     headers: self.authHeaders
   }, function (err, response, body) {
     if (err) return callback(err);
-    callback(null, JSON.parse(body));
+    callback(null, JSON.parse(body).users[0]);
+  });
+}
+
+CobudgetClient.prototype.getGroup = function (id, callback) {
+  var self = this
+  request.get({
+    uri: self.baseUri + '/groups/' + id,
+    headers: self.authHeaders
+  }, function (err, response, body) {
+    if (err) return callback(err);
+    callback(null, JSON.parse(body).groups[0]);
+  });
+}
+
+CobudgetClient.prototype.getBucketsForGroup = function (group, callback) {
+  var self = this
+  request.get({
+    uri: self.baseUri + '/buckets?group_id=' + group.id,
+    headers: self.authHeaders
+  }, function (err, response, body) {
+    if (err) return callback(err);
+    callback(null, JSON.parse(body).buckets);
+  });
+}
+
+CobudgetClient.prototype.getAllocationsForGroup = function (group, callback) {
+  var self = this
+  request.get({
+    uri: self.baseUri + '/allocations?group_id=' + group.id,
+    headers: self.authHeaders
+  }, function (err, response, body) {
+    if (err) return callback(err);
+    callback(null, JSON.parse(body).allocations);
+  });
+}
+
+CobudgetClient.prototype.getContributionsForBucket = function (bucket, callback) {
+  var self = this
+  request.get({
+    uri: self.baseUri + '/contributions?bucket_id=' + bucket.id,
+    headers: self.authHeaders
+  }, function (err, response, body) {
+    if (err) return callback(err);
+    callback(null, JSON.parse(body).contributions);
+  });
+}
+
+CobudgetClient.prototype.getCommentsForBucket = function (bucket, callback) {
+  var self = this
+  request.get({
+    uri: self.baseUri + '/comments?bucket_id=' + bucket.id,
+    headers: self.authHeaders
+  }, function (err, response, body) {
+    if (err) return callback(err);
+    callback(null, JSON.parse(body).comments);
   });
 }
